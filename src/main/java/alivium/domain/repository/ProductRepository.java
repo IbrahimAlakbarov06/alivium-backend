f7cea98b9b6ae7c19326bf90cda74c9af3968f0e
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-12-07 06:53:29 +0400

    Added methods and new repo

--- src/main/java/alivium/domain/repository/ProductRepository.java
+++ src/main/java/alivium/domain/repository/ProductRepository.java
@@ -1,10 +1,20 @@
 package alivium.domain.repository;
 
+import alivium.domain.entity.Category;
+import alivium.domain.entity.Collection;
 import alivium.domain.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
     boolean existsByName(String name);
+
+    List<Product> findAllByActiveTrue();
+
+    List<Product> findByCategoriesContainingAndActiveTrue(Category category);
+
+    List<Product> findByCollectionsContainingAndActiveTrue(Collection collection);
 }
