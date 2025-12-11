66f6e83338e79b2c082202b8652cbfb23fbe64c3
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-12-11 20:57:18 +0400

    removing methods from payment repo

--- src/main/java/alivium/domain/repository/PaymentCardRepository.java
+++ src/main/java/alivium/domain/repository/PaymentCardRepository.java
@@ -4,21 +4,6 @@ import alivium.domain.entity.PaymentCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long> {
-
-    List<PaymentCard> findByUserId(Long userId);
-
-    Optional<PaymentCard> findByIdAndUserId(Long id, Long userId);
-
-    List<PaymentCard> findByUserIdAndBrand(Long userId, String brand);
-
-    Optional<PaymentCard> findByUserIdAndIsDefaultTrue(Long userId);
-
-    boolean existsByUserIdAndLast4Digits(Long userId, String last4Digits);
-
-    int countByUserId(Long userId);
 }
