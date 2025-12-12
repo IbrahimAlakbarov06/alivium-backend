d9791095fe181b2af95a4086374acb8c6be1aa5e
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-12-13 02:16:47 +0400

    minio configuration+service

---
+++ src/main/java/alivium/config/MinioProperties.java
@@ -0,0 +1,22 @@
+package alivium.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "minio")
+public class MinioProperties {
+    private String endpoint;
+    private String accessKey;
+    private String secretKey;
+    private Bucket bucket;
+
+    @Data
+    public static class Bucket {
+        private String product;
+        private String review;
+    }
+
+}
