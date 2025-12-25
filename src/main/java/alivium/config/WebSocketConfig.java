c5336b21a537681d822e388960065960f251d3e8
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-12-25 22:42:43 +0400

    WebSocketConfig updated

--- src/main/java/alivium/config/WebSocketConfig.java
+++ src/main/java/alivium/config/WebSocketConfig.java
@@ -13,13 +13,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/queue");
-        registry.setUserDestinationPrefix("/app");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
