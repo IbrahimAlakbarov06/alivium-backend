7ccaa60af39ca129c61811b3ea095149fb88128f
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2026-02-07 23:37:59 +0400

    cancel request added for order

---
+++ src/main/java/alivium/model/dto/request/CancelOrderRequest.java
@@ -0,0 +1,20 @@
+package alivium.model.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CancelOrderRequest {
+
+    @NotBlank(message = "Cancellation reason is required")
+    @Size(min = 10, max = 500, message = "Reason must be between 10 and 500 characters")
+    private String reason;
+}
