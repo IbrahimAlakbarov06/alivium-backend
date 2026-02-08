571637d7ef73f4b75c457c24017b162db3a5d0c7
DilbarMirzaeva <dlbarmirza@gmail.com>
2026-02-08 23:57:59 +0400

    PaymentCard entity+repo

--- src/main/java/alivium/domain/entity/User.java
+++ src/main/java/alivium/domain/entity/User.java
@@ -64,8 +64,8 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Address> addresses = new HashSet<>();
 
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private Set<PaymentCard> paymentCards = new HashSet<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<PaymentCard> paymentCards = new HashSet<>();
 //
 //    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
 //    private Set<Order> orders = new HashSet<>();
