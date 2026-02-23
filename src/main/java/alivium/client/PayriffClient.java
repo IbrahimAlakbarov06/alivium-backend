59a52688720ea2435c273e95f383d23ec3a3574a
DilbarMirzaeva <dlbarmirza@gmail.com>
2026-02-23 23:58:02 +0400

    PayriffClient

--- src/main/java/alivium/client/PayriffClient.java
+++ src/main/java/alivium/client/PayriffClient.java
@@ -1,9 +1,14 @@
 package alivium.client;
 
 import alivium.config.PayriffFeignConfig;
+import alivium.model.dto.request.PayriffAutoPayRequest;
 import alivium.model.dto.request.PayriffCreateOrderRequest;
+import alivium.model.dto.response.PayriffAutoPayResponse;
 import alivium.model.dto.response.PayriffCreateOrderResponse;
+import alivium.model.dto.response.PayriffOrderInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,6 +17,12 @@ import org.springframework.web.bind.annotation.RequestBody;
         ,configuration = PayriffFeignConfig.class)
 public interface PayriffClient {
 
-    @PostMapping("/api/v3/orders")
+    @PostMapping("/orders")
     PayriffCreateOrderResponse createOrder(@RequestBody PayriffCreateOrderRequest payriffCreateOrderRequest);
+
+    @PostMapping("/autoPay")
+    PayriffAutoPayResponse autoPay(@RequestBody PayriffAutoPayRequest payRequest);
+
+    @GetMapping("/orders/{orderId}")
+    PayriffOrderInfoResponse orderInfo(@PathVariable String orderId);
 }
